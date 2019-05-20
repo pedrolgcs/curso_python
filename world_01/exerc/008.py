7675ab9f6ed2cc832a8696766b717c87652d1b69
Pedro Henrique <pedro.lg.cs@gmail.com>
2019-05-20 00:41:37 -0300

    text functions

--- world_01/exerc/008.py
+++
@@ -1,6 +0,0 @@
-import pygame
-
-pygame.init()
-pygame.mixer.music.load('./008.mp3')
-pygame.mixer.music.play()
-pygame.event.wait()
---
+++ world_01/manipante_text.py
@@ -0,0 +1,5 @@
+frase = 'Curso em video Python'
+
+print(frase[9])
+
+# 7:08
